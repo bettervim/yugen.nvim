eacdd67d4b3d3ea904c18ed1fd9bb5a5136f1a69
Gatin <44415584+GatinSDK@users.noreply.github.com>
2025-05-01 13:39:27 +0000

    fix(colors): change opts names

--- lua/yugen/init.lua
+++ lua/yugen/init.lua
@@ -5,8 +5,8 @@ local M = {}
 local config = {
   bold_vert_split = false,
   dark_variant = 'main',
-  disable_background = false,
-  disable_float_background = false,
+  transparent = false,
+  statusline_transparent = false,
   disable_italics = false,
   dim_nc_background = false,
 
